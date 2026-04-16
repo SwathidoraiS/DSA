44fe40be8f0668044322060e3cd0dd19121d5477
Swathi S <priyaswathi2119@gmail.com>
2026-04-16 23:41:38 +0900

    Array- Group Anagrams

---
+++ array/GroupAnagrams.java
@@ -0,0 +1,43 @@
+package array;
+
+// Day -4
+
+// Group Anagrams
+
+/* Description
+Given an array of strings strs, group all anagrams together into sublists. You may return the output in any order.
+
+An anagram is a string that contains the exact same characters as another string, but the order of the characters can be different.
+
+Example 1:
+
+Input: strs = ["act","pots","tops","cat","stop","hat"]
+
+Output: [["hat"],["act", "cat"],["stop", "pots", "tops"]] */
+
+
+/** Time Complexity: O(n * k log k), Space Complexity: O(n) where n is the number of strings and k is the maximum length of a string in the input array. */
+
+// 1. Hash Map
+
+import java.util.*;
+
+class GroupAnagrams {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        
+        for (String word : strs) {
+            char[] chars = word.toCharArray();
+            Arrays.sort(chars);
+            String sortedWord = new String(chars);
+            
+            if (!map.containsKey(sortedWord)) {
+                map.put(sortedWord, new ArrayList<>());
+            }
+            
+            map.get(sortedWord).add(word);
+        }
+        
+        return new ArrayList<>(map.values());
+    }
+}
